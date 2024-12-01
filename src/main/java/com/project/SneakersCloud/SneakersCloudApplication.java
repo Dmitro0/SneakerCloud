7db638c9d8f3df4a6e6ff0125931a06712a33252
Dmitriy <klin234@inbox.ru>
2024-12-01 20:12:46 +0300

    Creating

---
+++ src/main/java/com/project/SneakersCloud/SneakersCloudApplication.java
@@ -0,0 +1,13 @@
+package com.project.SneakersCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakersCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakersCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/SneakersCloud/SneakersCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SneakersCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakersCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
